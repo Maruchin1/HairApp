524740a95713fccae1c321357ffa9fd637a1e799
Maruchin1 <marcinpk.mp@gmail.com>
2021-05-21 22:15:35 +0200

    updated CareSchemaDaoTest.kt

--- corev2/src/androidTest/java/com/example/corev2/dao/CareSchemaDaoTest.kt
+++ corev2/src/androidTest/java/com/example/corev2/dao/CareSchemaDaoTest.kt
@@ -2,6 +2,8 @@ package com.example.corev2.dao
 
 import com.example.corev2.HairAppDatabaseTestRule
 import com.example.corev2.entities.CareSchema
+import com.example.corev2.entities.CareSchemaStep
+import com.example.corev2.entities.ProductType
 import com.example.corev2.relations.CareSchemaWithSteps
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.flow.first
@@ -16,19 +18,57 @@ class CareSchemaDaoTest {
     val hairAppDatabaseTestRule = HairAppDatabaseTestRule()
 
     private val careSchemaDao by lazy { hairAppDatabaseTestRule.db.careSchemaDao() }
+    private val careSchemaStepDao by lazy { hairAppDatabaseTestRule.db.careSchemaStepDao() }
+
+    private val omoSchema = CareSchema(
+        id = 0,
+        name = "OMO"
+    )
+    private val cgSchema = CareSchema(
+        id = 0,
+        name = "CG"
+    )
+    private val omoSteps = arrayOf(
+        CareSchemaStep(
+            id = 0,
+            prouctType = ProductType.CONDITIONER,
+            order = 0,
+            careSchemaId = 1
+        ),
+        CareSchemaStep(
+            id = 0,
+            prouctType = ProductType.SHAMPOO,
+            order = 1,
+            careSchemaId = 1
+        ),
+        CareSchemaStep(
+            id = 0,
+            prouctType = ProductType.CONDITIONER,
+            order = 2,
+            careSchemaId = 1
+        ),
+    )
+    private val cgSteps = arrayOf(
+        CareSchemaStep(
+            id = 0,
+            prouctType = ProductType.CONDITIONER,
+            order = 0,
+            careSchemaId = 2
+        ),
+        CareSchemaStep(
+            id = 0,
+            prouctType = ProductType.CONDITIONER,
+            order = 1,
+            careSchemaId = 2
+        )
+    )
 
     @Before
     fun before() {
-        val firstSchema = CareSchema(
-            id = 0,
-            name = "Schema 1"
-        )
-        val secondSchema = CareSchema(
-            id = 0,
-            name = "Schema 2"
-        )
         runBlocking {
-            careSchemaDao.insert(firstSchema, secondSchema)
+            careSchemaDao.insert(omoSchema, cgSchema)
+            careSchemaStepDao.insert(*omoSteps)
+            careSchemaStepDao.insert(*cgSteps)
         }
     }
 
@@ -38,18 +78,19 @@ class CareSchemaDaoTest {
 
         assertThat(result).containsExactly(
             CareSchemaWithSteps(
-                careSchema = CareSchema(
-                    id = 1,
-                    name = "Schema 1"
-                ),
-                steps = listOf()
+                careSchema = omoSchema.copy(id = 1),
+                steps = listOf(
+                    omoSteps[0].copy(id = 1),
+                    omoSteps[1].copy(id = 2),
+                    omoSteps[2].copy(id = 3)
+                )
             ),
             CareSchemaWithSteps(
-                careSchema = CareSchema(
-                    id = 2,
-                    name = "Schema 2"
-                ),
-                steps = listOf()
+                careSchema = cgSchema.copy(id = 2),
+                steps = listOf(
+                    cgSteps[0].copy(id = 4),
+                    cgSteps[1].copy(id = 5),
+                )
             )
         )
         Unit
@@ -61,11 +102,12 @@ class CareSchemaDaoTest {
 
         assertThat(result).isEqualTo(
             CareSchemaWithSteps(
-                careSchema = CareSchema(
-                    id = 1,
-                    name = "Schema 1"
-                ),
-                steps = listOf()
+                careSchema = omoSchema.copy(id = 1),
+                steps = listOf(
+                    omoSteps[0].copy(id = 1),
+                    omoSteps[1].copy(id = 2),
+                    omoSteps[2].copy(id = 3)
+                )
             )
         )
     }
